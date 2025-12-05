c2c3fc36e808b26197cb5a74bbc6e6cffaa0fa27
lunalaviada-png <luna.laviada@gmail.com>
2025-12-05 10:18:00 -0600

    Update ball.c

--- src/ball.c
+++ src/ball.c
@@ -42,32 +42,7 @@ void ball_update() {
     if (ball.y <= 0)
         ball.dy = -ball.dy;
 
-    // colisión con la paleta
-    Paddle *p = paddle_get();
-    if (ball.y == 23) {
-        if (ball.x >= p->x - p->width/2 && ball.x <= p->x + p->width/2)
-            ball.dy = -ball.dy;
-    }
 
-    // colisiones con ladrillos
-    Brick (*b)[BRICK_COLS] = bricks_get();
-    for (int i = 0; i < BRICK_ROWS; i++) {
-        for (int j = 0; j < BRICK_COLS; j++) {
-            if (b[i][j].active) {
-
-                int brick_x = j * 8 + 4;
-                int brick_y = i * 2 + 2;
-
-                if (ball.x >= brick_x - 3 && ball.x <= brick_x + 3 &&
-                    ball.y == brick_y) {
-
-                    brick_destroy(i, j);
-                    ball.dy = -ball.dy;
-                    score_add(10);
-                }
-            }
-        }
-    }
 }
 
 Ball* ball_get() {
