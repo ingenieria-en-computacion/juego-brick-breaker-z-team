5c1e10b3cab20ed77ed8476f4c38fa2d3a892644
lunalaviada-png <luna.laviada@gmail.com>
2025-12-05 10:08:02 -0600

    Update ball.c

--- src/ball.c
+++ src/ball.c
@@ -1,4 +1,4 @@
-#include "ball.h"
+/*#include "ball.h"
 
 static Ball ball;
 
@@ -12,4 +12,22 @@ void ball_update() {
 
 Ball* ball_get() {
     return &ball;
-}
+}*/
+
+#ifndef BALL_H
+#define BALL_H
+
+typedef struct {
+    int x, y;       // Posición
+    int dx, dy;     // Dirección
+} Ball;
+
+void ball_init();
+void ball_update();
+
+// Devuelve un puntero a la pelota global
+Ball* ball_get();
+
+#endif
+
+
