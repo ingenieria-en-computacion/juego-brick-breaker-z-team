5b15eaa409e990163fb557c8e84b46591f0b8a8e
lunalaviada-png <luna.laviada@gmail.com>
2025-12-05 11:24:35 -0600

    Update ball.c

--- src/ball.c
+++ src/ball.c
@@ -13,7 +13,11 @@ void ball_update() {
 Ball* ball_get() {
     return &ball;
 }*/
+
 #include "ball.h"
+#include "brick.h"
+#include "paddle.h"
+#include "score.h"
 
 
 static Ball ball;   // pelota global
@@ -39,7 +43,29 @@ void ball_update() {
     // colisión con parte superior
     if (ball.y <= 0)
         ball.dy = -ball.dy;
-
+    
+    // colisión con la raqueta
+    Paddle *p = paddle_get();
+    if (ball.y == 23) {
+        if (ball.x >= p->x - p->width/2 && ball.x <= p->x + p->width/2)
+            ball.dy = -ball.dy;
+    }
+        // colisión con ladrillos
+    Brick (*b)[BRICK_COLS] = bricks_get();
+    for (int i=0; i<BRICK_ROWS; i++) {
+        for (int j=0; j<BRICK_COLS; j++) {
+            if (b[i][j].active) {
+                int brick_x = j*8 + 4;
+                int brick_y = i*2 + 2;
+                if (ball.x >= brick_x-3 && ball.x <= brick_x+3 &&
+                    ball.y == brick_y) {
+                    brick_destroy(i,j);
+                    ball.dy = -ball.dy;
+                    score_add(10);
+                    }
+                }
+            }
+        }
 
 }
 
