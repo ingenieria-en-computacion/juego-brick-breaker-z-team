72fe82c26add3933bbb8f6504df6d2c623ebed4e
lunalaviada-png <luna.laviada@gmail.com>
2025-12-05 10:23:11 -0600

    Update input.c

--- src/input.c
+++ src/input.c
@@ -1,9 +1,19 @@
 #include "input.h"
 #include "library_compat.h"
-
+/*
 int input_update() {
     // TODO:
     // Leer teclas 'a' y 'd', regresar -1 o 1
+    // Si no se presiona nada, regresar 0
+    return 0;
+}*/
+int input_update() {
+    // Leer teclas 'a' y 'd', regresar -1 o 1
+    if (!kbhit_compat()) return 0;   // ⚠ usar kbhit_compat
+    int c = getch_compat();          // correcto
+    if (c == 'a' || c == 'A') return -1;
+    if (c == 'd' || c == 'D') return 1;
+
     // Si no se presiona nada, regresar 0
     return 0;
 }
